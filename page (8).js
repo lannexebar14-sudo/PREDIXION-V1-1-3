a982d5b34ddcc9923db98d28405552e22238814e
lannexebar14-sudo <lannexebar14@gmail.com>
2026-07-12 22:17:07 +0200

    Delete page (8).js

--- page (8).js
+++
@@ -1,5 +0,0 @@
-import GenericPage from "../components/GenericPage";
-
-export default function Page() {
-  return <GenericPage title='ÉVÉNEMENTS' subtitle='Pronostique sur des événements spéciaux et tendances.' cards=[{'title': 'Télévision', 'text': 'Pronostics sur les grands événements télévisés.', 'button': 'OUVRIR'}, {'title': 'Finance', 'text': 'Pronostics sur les évolutions de marché.', 'button': 'OUVRIR'}, {'title': 'Culture', 'text': 'Pronostics sur les sorties et récompenses.', 'button': 'OUVRIR'}] />;
-}
