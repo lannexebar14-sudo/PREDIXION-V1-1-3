19c8ca55b4045f0666c6b65f2708ab121b77fbe1
lannexebar14-sudo <lannexebar14@gmail.com>
2026-07-12 22:20:04 +0200

    Delete route.js

--- route.js
+++
@@ -1,5 +0,0 @@
-import GenericPage from "../components/GenericPage";
-
-export default function Page() {
-  return <GenericPage title='MES PARIS' subtitle='Retrouve tous tes paris en cours.' cards=[{'title': 'Paris actifs', 'text': 'Tes paris confirmés apparaîtront ici.', 'href': '/historique', 'button': 'VOIR L’HISTORIQUE'}, {'title': 'Nouveau pari', 'text': 'Retourne sur les matchs du jour pour placer un pari.', 'href': '/sports', 'button': 'PARIER'}] />;
-}
