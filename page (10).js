080d016b95afd54a414ede0c53e8f8f216a8a88e
lannexebar14-sudo <lannexebar14@gmail.com>
2026-07-12 22:17:14 +0200

    Delete page (10).js

--- page (10).js
+++
@@ -1,5 +0,0 @@
-import GenericPage from "../components/GenericPage";
-
-export default function Page() {
-  return <GenericPage title='CLASSEMENT' subtitle='Retrouve les meilleurs joueurs de la semaine.' cards=[{'title': '1. Baba M.', 'text': '125 450 crédits gagnés cette semaine.', 'button': 'VOIR LE PROFIL'}, {'title': '2. QueenB', 'text': '98 230 crédits gagnés cette semaine.', 'button': 'VOIR LE PROFIL'}, {'title': '3. Predator', 'text': '85 770 crédits gagnés cette semaine.', 'button': 'VOIR LE PROFIL'}] />;
-}
